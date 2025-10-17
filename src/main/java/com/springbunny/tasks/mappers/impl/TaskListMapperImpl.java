10f8f974c8ad42ac8ce9dbabae69c1f3593a4797
bunnyCoder <rahuljames669@gmail.com>
2025-10-17 11:20:29 +0530

    Defined the TaskListRepository and TaskRepository interfaces and also added notes on TaskMapperImpl class

--- src/main/java/com/springbunny/tasks/mappers/impl/TaskListMapperImpl.java
+++ src/main/java/com/springbunny/tasks/mappers/impl/TaskListMapperImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Component
 public class TaskListMapperImpl implements TaskListMapper {
 
-    private final TaskMapper taskMapper;
+    private final TaskMapper taskMapper; // taskMapper injected
 
     public TaskListMapperImpl(TaskMapper taskMapper) {
         this.taskMapper = taskMapper;
@@ -26,9 +26,9 @@ public class TaskListMapperImpl implements TaskListMapper {
                 taskListDto.id(),
                 taskListDto.title(),
                 taskListDto.description(),
-                Optional.ofNullable(taskListDto.tasks())
+                Optional.ofNullable(taskListDto.tasks()) // to return a list of tasks by taking in a taskListDto.
                         .map(tasks -> tasks.stream()
-                                .map(taskMapper::fromDto)
+                                .map(taskMapper::fromDto) //taskMapper injected
                                 .toList()
                         ).orElse(null),
                 null,
@@ -64,3 +64,10 @@ public class TaskListMapperImpl implements TaskListMapper {
         return(double) closedTaskCount / tasks.size();
     }
 }
+
+// The Optional.ofNullable() function safely wraps a value that might be null, preventing NullPointerExceptions.
+// If the value is non-null, it allows further operations (like map()); if null, it gracefully skips processing.
+// In this mapper, Optional.ofNullable() is used to handle potential null task lists or DTO task collections.
+// The map() function transforms each non-null list by streaming its elements, applying the taskMapper (fromDto or toDto) to convert between entities and DTOs.
+// The resulting stream is collected back into a List using .toList(), ensuring safe and clean data mapping.
+// TaskListImpl also contains implementations of calculateTaskListProgress
